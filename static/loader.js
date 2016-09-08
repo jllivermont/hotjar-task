63737a91b8633b647f56cc330975adfcc689b284
James Livermont <jimboliv@gmail.com>
2016-09-08 02:14:14 +0100

    Update settings in loader

--- static/loader.js
+++ static/loader.js
@@ -1,5 +1,13 @@
 function popup() {
-  var new_window = window.open('https://hotjar-task.herokuapp.com/widget', 'Survey','height=500,width=500');
+  var options = 'height=550,width=575';
+
+  // Figure out the coordinates to place the popup in the middle of the screen
+  var height = Math.floor(window.screen.height / 3);
+  var width = Math.floor(window.screen.width / 3);
+
+  options = options + ",top=" + height + ",left=" + width; 
+
+  var new_window = window.open('https://hotjar-task.herokuapp.com/widget', 'Survey', options);
   new_window.focus();
 }
 
