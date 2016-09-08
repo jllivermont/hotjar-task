825ba2670c163424a19894b67a8a333b015ad2af
James Livermont <jimboliv@gmail.com>
2016-09-09 00:24:56 +0100

    Add ACAC header for CORS

--- survey/service.py
+++ survey/service.py
@@ -159,6 +159,7 @@ class StaticResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.content_type = self.content_type
+        resp.set_headers({"Access-Control-Allow-Origin": "*"})
 
         path = os.path.join(os.environ["STATIC_CONTENT_PATH"], self.file_name)
         with open(path, 'r') as f:
